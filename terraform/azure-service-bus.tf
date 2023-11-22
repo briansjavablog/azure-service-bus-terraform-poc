a801c16aeff8dbb96cdfd097a5229026ac07cf65
Brians Computer <hannawaybrian@googlemail.com>
2023-11-22 10:19:48 +0000

    renamed original queue and created a second one

--- terraform/azure-service-bus.tf
+++ terraform/azure-service-bus.tf
@@ -14,7 +14,23 @@ resource "azurerm_servicebus_namespace" "asb-namespace" {
 }
 
 # Define the Service Bus Queue
-resource "azurerm_servicebus_queue" "test-queue" {
+resource "azurerm_servicebus_queue" "test-queue-1" {
+  namespace_id        = azurerm_servicebus_namespace.asb-namespace.id
+  name                = "sb-poc-queue"
+  enable_partitioning = true
+  enable_express = false # lazy writing of messages to disk - increases performance but doesn't guarantee delivery
+  dead_lettering_on_message_expiration = true # DL if not consumed inside TTL period
+  default_message_ttl = "PT10D"
+  lock_duration = "PT1M"
+  max_delivery_count = 5
+  max_message_size_in_kilobytes = 100
+  max_size_in_megabytes = 5120 # 5GB
+  requires_duplicate_detection = false
+  requires_session = false
+}
+
+# Define the Service Bus Queue
+resource "azurerm_servicebus_queue" "test-queue-2" {
   namespace_id        = azurerm_servicebus_namespace.asb-namespace.id
   name                = "sb-poc-queue"
   enable_partitioning = true
