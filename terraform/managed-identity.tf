0ee39c83618da1d1b9084ae4dc2f10fdcc05662c
Brians Computer <hannawaybrian@googlemail.com>
2023-11-22 16:59:52 +0000

    renamed MIs

--- terraform/managed-identity.tf
+++ terraform/managed-identity.tf
@@ -2,12 +2,12 @@
 resource "azurerm_user_assigned_identity" "microservice1_identity" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  name                = "service1-identity"
+  name                = "service1-mi"
 }
 
 # Define Managed Identity for microservice 2
 resource "azurerm_user_assigned_identity" "microservice2_identity" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  name                = "service2-identity"
+  name                = "service2-mi"
 }
