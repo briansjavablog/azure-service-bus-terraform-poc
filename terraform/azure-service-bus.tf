19a6203f1fe8945584009783978a53374b4ab685
Brians Computer <hannawaybrian@googlemail.com>
2023-11-22 15:46:00 +0000

    fixed queue 2 config

--- terraform/azure-service-bus.tf
+++ terraform/azure-service-bus.tf
@@ -36,7 +36,7 @@ resource "azurerm_servicebus_queue" "test-queue-2" {
   enable_partitioning = true
   enable_express = false # lazy writing of messages to disk - increases performance but doesn't guarantee delivery
   dead_lettering_on_message_expiration = true # DL if not consumed inside TTL period
-  default_message_ttl = "PT10D"
+  default_message_ttl = "P10D"
   lock_duration = "PT1M"
   max_delivery_count = 5
   max_message_size_in_kilobytes = 100
