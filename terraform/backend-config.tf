a08a1ebb1c11ff90e5a7106f88daac284cdd1e33
Brians Computer <hannawaybrian@googlemail.com>
2023-11-22 08:11:41 +0000

    updated backend resource group

--- terraform/backend-config.tf
+++ terraform/backend-config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "asb-poc-rg"
+    resource_group_name   = "iac-poc-rg"
     storage_account_name  = "iacpocstorageaccount"
     container_name        = "asb-poc-terraform-state"
     key                   = "terraform.tfstate"
