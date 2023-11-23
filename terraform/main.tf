f18cd4416a1439eb63fb9db2e9e9ee849f1901ef
Brians Computer <hannawaybrian@googlemail.com>
2023-11-23 16:20:35 +0000

    added MI Client ID to terraform provider def

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 # Configure the Azure Provider
 provider "azurerm" {
   features {}
+  client_id = "c7516409-8113-41d8-b994-6201c16a543a"
   # If you are running this on a machine where the Managed Identity has been assigned,
   # Terraform will automatically use this identity and you don't need to specify credentials here.
 }
