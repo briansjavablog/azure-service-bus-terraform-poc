d3cfe6606f95ba320a55e03cb046eea23b190abb
Brians Computer <hannawaybrian@googlemail.com>
2023-11-23 17:30:34 +0000

    updates

--- terraform/azure-function.tf
+++ terraform/azure-function.tf
@@ -3,8 +3,8 @@ resource "azurerm_function_app" "asb_test-function-app" {
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.azure-function-storage-account.name
-  storage_account_access_key = azurerm_storage_account.azure-function-storage-account.primary_access_key
+  storage_account_name       = azurerm_storage_account.function-storage-account.name
+  storage_account_access_key = azurerm_storage_account.function-storage-account.primary_access_key
   os_type                    = "linux"
   functions_extension_version = "~3"
 
@@ -27,8 +27,8 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_storage_account" "azure-function-storage-account" {
-  name                     = "azure-function-storage-account"
+resource "azurerm_storage_account" "function-storage-account" {
+  name                     = "function-storage-account"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
