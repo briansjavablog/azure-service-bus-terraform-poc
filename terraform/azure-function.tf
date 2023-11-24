a3f708007b6f9785ed059c06e6326f742f8be2df
Brians Computer <hannawaybrian@googlemail.com>
2023-11-24 10:02:22 +0000

    updates

--- terraform/azure-function.tf
+++ terraform/azure-function.tf
@@ -28,7 +28,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_storage_account" "funct-storage-account" {
-  name                     = "funct-storage-account"
+  name                     = "functionstorageaccount"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
